4eda501cc957e9ff107cd5ee37783bd57d707c47
Vembarasan N <vembarasugn@gmail.com>
2022-07-20 22:56:29 +0530

    Google oauth2.0 fix

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,12 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const User = require('../models/User')
 
+
 module.exports = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL:'http://localhost:3000/auth/google/callback'
+        callbackURL:process.env.callURI +'/auth/google/callback'
     },
     async (accessToken,refreshToken,profile,done) => {
         const newUser = {
